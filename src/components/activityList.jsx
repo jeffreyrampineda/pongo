1d7aa78e6e807c8583e8589348fd5acd4f798e75
jeffreyrampineda <jeffreyrampineda@gmail.com>
2018-12-08 16:44:13 -0500

    improved activityList with isEmpty condition

--- src/components/activityList.jsx
+++ src/components/activityList.jsx
@@ -3,18 +3,33 @@ import Activity from './activity';
 
 class ActivityList extends Component {
   render() {
+    const isEmpty = this.props.activities.length === 0;
+    let list;
+
+    if (isEmpty) {
+      list = 
+      <div style={{textAlign: "center"}}>
+        <h1 className="mt-5">No activities!</h1>
+      </div>;
+    } else {
+      list =
+        <div class="list-group">
+          {this.props.activities.map(activity =>
+            <Activity
+              key={activity.id}
+              id={activity.id}
+              title={activity.title}
+              datetime={activity.datetime}
+              onDelete={this.props.onDelete}
+            />
+          )}
+        </div>;
+    }
+
     return (
-      <div class="list-group">
-        {this.props.activities.map(activity =>
-          <Activity
-            key={activity.id}
-            id={activity.id}
-            title={activity.title}
-            datetime={activity.datetime}
-            onDelete={this.props.onDelete}
-          />
-        )}
-      </div>
+      <React.Fragment>
+        {list}
+      </React.Fragment>
     );
   }
 }
