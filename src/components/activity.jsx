eba25ff4b8a832e8bf66ace3a9c01af6b88ede64
jeffreyrampineda <jeffreyrampineda@gmail.com>
2018-12-07 23:12:53 -0500

    added date formatting

--- src/components/activity.jsx
+++ src/components/activity.jsx
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+var moment = require('moment');
 
 class Activity extends Component {
+
+  howLongAgo() {
+    const day = 1440;
+    const hour = 60;
+    const diff = Math.abs(new Date() - this.props.datetime);
+    const minutes = Math.floor((diff / 1000) / 60);
+    var timeAgoString = "";
+
+    // Days
+    if (minutes >= day) {
+      var days = Math.floor(minutes / day);
+      timeAgoString = days + " day" + this.pluralize(days)
+
+    // Hours
+    } else if (minutes >= hour) {
+      var hours = Math.floor(minutes / hour);
+      timeAgoString = hours + " hour" + this.pluralize(hours);
+      
+    // Minutes
+    } else {
+      timeAgoString = minutes + " minute" + this.pluralize(minutes);
+    }
+
+    return timeAgoString + " ago";
+  }
+
+  pluralize(number) {
+    return number >= 2 ? "s" : "";
+  }
+
   render() {
     return (
       <div>
-
+        <div>{this.howLongAgo()}</div>
+        <div>
+          <div>{this.props.title}</div>
+          <div>{moment(this.props.datetime).format('DD/MM/YY, HH:mm')}</div>
+        </div>
       </div>
     );
   }
--- src/components/activityList.jsx
+++ src/components/activityList.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import Activity from './activity';
 
 class ActivityList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      activities: [{
+          title: "test title",
+          datetime: new Date('2018/12/05 19:20'),
+        }, {
+          title: "test title 2",
+          datetime: new Date('2018/12/06 19:20'),
+        },
+      ]
+    };
+  }
+
+
   render() {
     return (
       <div>
-
+        {this.state.activities.map(activity => <Activity title={activity.title} datetime={activity.datetime}/>)}
       </div>
     );
   }
