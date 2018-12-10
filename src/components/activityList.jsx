39df1557e872da0b98747899a6fa794728ebf56c
jeffreyrampineda <jeffreyrampineda@gmail.com>
2018-12-10 11:15:32 -0500

    added sort by date function

--- src/components/activityList.jsx
+++ src/components/activityList.jsx
@@ -5,16 +5,21 @@ class ActivityList extends Component {
   render() {
     const isEmpty = this.props.activities.length === 0;
     let list;
-
+    
     if (isEmpty) {
-      list = 
-      <div style={{textAlign: "center"}}>
-        <h1 className="mt-5">No activities!</h1>
-      </div>;
-    } else {
       list =
-        <div class="list-group">
-          {this.props.activities.map(activity =>
+        <div style={{ textAlign: "center" }}>
+          <h1 className="mt-5">No activities!</h1>
+        </div>;
+    } else {
+
+      // Sort activities list by date then map
+      // each activities into individual components
+      list =
+        <div className="list-group">
+          {this.props.activities.sort(
+            (a, b) => { return new Date(b.datetime) - new Date(a.datetime) }
+          ).map(activity =>
             <Activity
               key={activity.id}
               id={activity.id}
