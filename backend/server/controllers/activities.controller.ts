ec3cd63abe4278994014be737c1c28a9a4cdede4
jeffreyrampineda <jeffreyrampineda@gmail.com>
2019-01-07 23:01:24 -0500

    added all CRUD endpoints

--- backend/server/controllers/activities.controller.ts
+++ backend/server/controllers/activities.controller.ts
@@ -6,8 +6,20 @@ class ActivitiesController {
         ctx.body = await Activity.find();
     }
 
+    async getById(ctx) {
+        ctx.body = await Activity.findOne({ _id: ctx.params.id })
+    }
+
     async create(ctx) {
         ctx.body = await Activity.create(ctx.request.body);
+    }
+
+    async update(ctx) {
+        ctx.body = await Activity.findOneAndUpdate({ _id: ctx.params.id }, ctx.request.body)
+    }
+
+    async delete(ctx) {
+        ctx.body = await Activity.deleteOne({ _id: ctx.params.id })
     }
 }
 
--- backend/server/routes/activities.routes.ts
+++ backend/server/routes/activities.routes.ts
@@ -4,6 +4,9 @@ import ActivitiesController from '../controllers/activities.controller';
 const router = new Router();
 
 router.get('/', ActivitiesController.getAll)
+router.get('/:id', ActivitiesController.getById)
 router.post('/', ActivitiesController.create)
+router.put('/:id', ActivitiesController.update)
+router.del('/:id', ActivitiesController.delete)
 
 export const ActivitiesRoute = router.routes();
