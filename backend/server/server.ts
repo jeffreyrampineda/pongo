c2ccdf5408eff3b7cbc3887ace08d5965f3d8631
jeffreyrampineda <jeffreyrampineda@gmail.com>
2019-01-25 16:47:27 -0500

    renamed project to pongo

--- backend/server/server.ts
+++ backend/server/server.ts
@@ -24,7 +24,7 @@ app.use(cors(options));
 app.use(bodyParser());
 app.use(router.routes());
 
-mongoose.connect(`mongodb://${db_host}:27017/pongo-log`, { useNewUrlParser: true });
+mongoose.connect(`mongodb://${db_host}:27017/pongo`, { useNewUrlParser: true });
 mongoose.connection.on('error', console.error);
 mongoose.connection.once('open', () => console.log('Connection to mongodb established'));
 
