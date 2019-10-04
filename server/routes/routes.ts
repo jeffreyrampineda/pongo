b0689a2ef467afd94a6398c4325e39894d766f61
jeffrey ram <jeffreyrampineda@gmail.com>
2019-10-04 15:22:38 -0400

    isolated production route

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,13 +1,5 @@
 import { ActivitiesRoute } from './activities.routes';
-const sendfile = require('koa-sendfile')
-const path = require("path")
 
 export default (router) => {
     router.use('/api/activities', ActivitiesRoute)
-
-    if (process.env.NODE_ENV === 'production') {
-        router.get('*', async (ctx) => {
-            await sendfile(ctx, (path.join(__dirname, "/../../", "frontend/build", "index.html")));
-        })
-    }
 }
--- server/server.ts
+++ server/server.ts
@@ -3,21 +3,27 @@ import * as Router from 'koa-router';
 import * as mongoose from 'mongoose';
 import * as bodyParser from 'koa-bodyparser';
 import routes from './routes/routes';
+const sendfile = require('koa-sendfile')
+const path = require("path")
 
 // Create Koa Application
 const app = new Koa();
 const router = new Router();
+
+routes(router)
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 
 } else if (process.env.NODE_ENV === 'production') {
     app.use(require('koa-static')(__dirname + '/../frontend/build'));
+    router.get('*', async (ctx) => {
+        await sendfile(ctx, (path.join(__dirname, "/../", "frontend/build", "index.html")));
+    })
 }
 
 const port = process.env.PORT;
 const mognodb_uri = process.env.MONGODB_URI;
-routes(router)
 
 app.use(bodyParser());
 app.use(router.routes());
