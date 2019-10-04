b3804cc83adfbee28723de9e994848d876117a8d
jeffrey ram <jeffreyrampineda@gmail.com>
2019-10-04 02:25:31 -0400

    removed cors

--- server/server.ts
+++ server/server.ts
@@ -1,28 +1,17 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import * as mongoose from 'mongoose';
-import * as cors from '@koa/cors';
 import * as bodyParser from 'koa-bodyparser';
 import routes from './routes/routes';
 
 // Create Koa Application
 const app = new Koa();
-const port = process.env.PORT || 3001
-const mognodb_uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/pongo';
-const API_URL = "http://localhost:3000"
-const options:cors.CorsOptions = {
-    allowHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
-    credentials: true,
-    allowMethods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-    origin: API_URL
-};
 const router = new Router();
 
 app.use(require('koa-static')(__dirname + '/../frontend/build'));
 
 routes(router)
 
-app.use(cors(options));
 app.use(bodyParser());
 app.use(router.routes());
 
