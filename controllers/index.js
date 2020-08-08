8c2c3c5f14379ce9e377ddce501dbfdc005e70c7
jeffrey ram pineda <jeffreyrampineda@gmail.com>
2020-08-08 12:44:53 -0400

    preparing: koajs->flask

--- controllers/index.js
+++
@@ -1,8 +0,0 @@
-const sendfile = require('koa-sendfile');
-
-module.exports = (router) => {
-    router.use('/api/activities', require('./activities.controller'));
-    router.get('(.*)', async (ctx) => {
-        await sendfile(ctx, __dirname + `/../client/build/index.html`);
-    });
-};
